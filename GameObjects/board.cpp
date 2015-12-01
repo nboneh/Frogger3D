d767dca4c29c9d2275d552d8cec3baed78a715b5
Nir Boneh <nboneh5921@gmail.com>
2015-11-30 18:44:29 -0700

    Fixing light

--- GameObjects/board.cpp
+++ GameObjects/board.cpp
@@ -2,6 +2,48 @@
 #include "WinningRow.h"
 #include <string>
 
+
+float distance  =   15;  // Light distance
+int smooth    =   1;  // Smooth/Flat shading
+int local     =   0;  // Local Viewer Model
+int ambient   =  30;  // Ambient intensity (%)
+int diffuse   = 100;  // Diffuse intensity (%)
+int specular  =   0;  // Specular intensity (%)
+int shininess =   0;  // Shininess (power of two)
+float shinyvec[1];    // Shininess (value)
+double zh =  330;  // Light azimuth
+float ylight  =   5;  // Elevation of light
+
+static void drawLight(){
+	  //  Translate intensity to color vectors
+   float Ambient[]   = {0.01*ambient ,0.01*ambient ,0.01*ambient ,1.0};
+   float Diffuse[]   = {0.01*diffuse ,0.01*diffuse ,0.01*diffuse ,1.0};
+   float Specular[]  = {0.01*specular,0.01*specular,0.01*specular,1.0};
+   //  Light position
+
+   float Position[]  = {distance*Cos(zh),ylight,distance*Sin(zh),1.0};
+    //  Draw light position as ball (still no lighting here)
+   glColor3f(1,1,1);
+   glPushMatrix();
+   glTranslatef(Position[0],Position[1],Position[2]);
+   glScalef(.1, .1,.1);
+   drawBall();
+   glPopMatrix();
+   //  OpenGL should normalize normal vectors
+   glEnable(GL_NORMALIZE);
+   //  Enable lighting
+   glEnable(GL_LIGHTING);
+   //  glColor sets ambient and diffuse color materials
+   glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
+   glEnable(GL_COLOR_MATERIAL);
+   //  Enable light 0
+   glEnable(GL_LIGHT0);
+   //  Set ambient, diffuse, specular components and position of light 0
+   glLightfv(GL_LIGHT0,GL_AMBIENT ,Ambient);
+   glLightfv(GL_LIGHT0,GL_DIFFUSE ,Diffuse);
+   glLightfv(GL_LIGHT0,GL_SPECULAR,Specular);
+   glLightfv(GL_LIGHT0,GL_POSITION,Position);
+}
 Board::Board(){
 	std::vector<unsigned int> water;
 	water.push_back(WATER4);
@@ -143,7 +185,7 @@ void Board::draw(){
     glVertex3f(ROW_WIDTH,1, -1);
     glEnd();
 
-
+    drawLight();
 
  	glPopMatrix();
 
